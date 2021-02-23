b39f8fe74c213866279dc0843b00f903259a61a8
Nermeen Salah <NERMEENS@eg.ibm.com>
2021-02-23 21:57:55 +0200

    fix get todo issues

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
@@ -6,11 +6,11 @@ import { createLogger } from '../../utils/logger'
 import { getUserId } from '../utils'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares';
-import { TodoItem } from '../../models/TodoItem'
+import { TodoItem } from '../../models/TodoItem';
 
 const client = new AWS.DynamoDB.DocumentClient();
 const todosTable = process.env.TODOS_TABLE;
- const indexTable=process.env.INDEX_NAME;
+ 
 const logger = createLogger('Get Todo');
 
 
@@ -20,10 +20,9 @@ export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGat
   logger.info('retrive todo items');
 
   const userId = getUserId(event);
-  
+ //const userId= 'google-oauth2|103198425751808458594';
   const result = await client.query({ 
     TableName: todosTable, // name of base table
-    IndexName : indexTable,
     KeyConditionExpression: 'userId = :userId', 
     ExpressionAttributeValues: { ':userId': userId },
   }).promise();
